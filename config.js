185fbe691d9de97a2e3b91513450d1d5ffd73b0d
Robert Edwards <robertedwardsmail@gmail.com>
2018-12-24 14:02:02 +0200

    Rate limiter

--- config.js
+++ config.js
@@ -80,7 +80,7 @@ var config = {
   pushNotificationsOpts: {
     templatePath: './lib/templates',
     defaultLanguage: 'en',
-    defaultUnit: 'btc',
+    defaultUnit: 'part',
     subjectPrefix: '',
     pushServerUrl: 'https://fcm.googleapis.com/fcm',
     authorizationKey: 'You_have_to_put_something_here',
